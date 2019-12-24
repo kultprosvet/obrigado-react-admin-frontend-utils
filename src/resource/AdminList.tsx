61e35b147f3b6dd78b9fc12b1a92a11abd852f9e
Anastasia Tsyagnok <anastasia.t@kultprosvet.net>
2019-12-24 13:39:42 +0200

    YM-143 remove export option from list toolbar

--- src/resource/AdminList.tsx
+++ src/resource/AdminList.tsx
@@ -1,23 +1,24 @@
 //@ts-ignore
-import * as React from 'react'
+import * as React from "react";
 //@ts-ignore
-import { List, Datagrid, TextField , Filter,TextInput} from 'react-admin'
-const AdminsFilter = (props:any) => (
-    <Filter {...props}>
-        <TextInput label="Search" source="q"  alwaysOn resettable />
-    </Filter>
+import { List, Datagrid, TextField, Filter, TextInput } from "react-admin";
+const AdminsFilter = (props: any) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn resettable />
+  </Filter>
 );
-export const AdminList = (props:any) => (
-    <List {...props}
-          filter={{graphql_fields:"id,username,last_name,first_name"}}
-          filters={<AdminsFilter/>}
-
-    >
-        <Datagrid  rowClick="edit">
-            <TextField source="id" />
-            <TextField source="username" title={"User name"} />
-            <TextField source="last_name" title={"Last name"} />
-            <TextField source="first_name" title={"First name"} />
-        </Datagrid>
-    </List>
-)
+export const AdminList = (props: any) => (
+  <List
+    {...props}
+    filter={{ graphql_fields: "id,username,last_name,first_name" }}
+    filters={<AdminsFilter />}
+    exporter={false}
+  >
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="username" title={"User name"} />
+      <TextField source="last_name" title={"Last name"} />
+      <TextField source="first_name" title={"First name"} />
+    </Datagrid>
+  </List>
+);
