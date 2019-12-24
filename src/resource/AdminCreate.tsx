6d7f67d3cea31aeb78ea1276e904be649023117e
Anastasia Tsyagnok <anastasia.t@kultprosvet.net>
2019-12-24 13:56:28 +0200

    YM-143 fix warnings

--- src/resource/AdminCreate.tsx
+++ src/resource/AdminCreate.tsx
@@ -1,32 +1,44 @@
 //@ts-ignore
-import * as React from 'react';
+import * as React from "react";
 //@ts-ignore
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 //@ts-ignore prettier-ignore
-import {SimpleForm, Create, TextInput, SelectInput, required} from 'react-admin'
+import {
+  SimpleForm,
+  Create,
+  TextInput,
+  SelectInput,
+  required
+  //@ts-ignore
+} from "react-admin";
 import config from "../config";
 
-export const AdminCreate=(props:any) => {
-    const [roles, setRoles] = useState([]);
-    useEffect(() => {
-        config.getRolesList().then(data => setRoles(data));
-    }, [])
-    const choices = roles.map(role => {
-        let choice = {
-            id: role,
-            name: role.charAt(0).toUpperCase()+role.substring(1).toLowerCase();
-        }
-        return choice;
-    });
-    return (
-        <Create {...props}>
-            <SimpleForm redirect='list'>
-                <TextInput source={'username'} validate={required()} />
-                <TextInput source={'last_name'} />
-                <TextInput source={'first_name'} />
-                <TextInput source={'password'} type={'password'} validate={required()} />
-                <SelectInput source={'role'} choices={choices} valiadte={required()} />
-            </SimpleForm>
-        </Create>
-    )
-}
+export const AdminCreate = (props: any) => {
+  const [roles, setRoles] = useState([]);
+  useEffect(() => {
+    config.getRolesList().then(data => setRoles(data));
+  }, []);
+  //@ts-ignore
+  const choices = roles.map(role => {
+    let choice = {
+      id: role,
+      name: role.charAt(0).toUpperCase() + role.substring(1).toLowerCase()
+    };
+    return choice;
+  });
+  return (
+    <Create {...props}>
+      <SimpleForm redirect="list">
+        <TextInput source={"username"} validate={required()} />
+        <TextInput source={"last_name"} />
+        <TextInput source={"first_name"} />
+        <TextInput
+          source={"password"}
+          type={"password"}
+          validate={required()}
+        />
+        <SelectInput source={"role"} choices={choices} valiadte={required()} />
+      </SimpleForm>
+    </Create>
+  );
+};
--- src/resource/AdminEdit.tsx
+++ src/resource/AdminEdit.tsx
@@ -1,33 +1,39 @@
 //@ts-ignore
-import * as React from 'react';
+import * as React from "react";
 //@ts-ignore
-import { useState, useEffect } from 'react';
-//@ts-ignore
-import {SimpleForm, Edit, TextInput, SelectInput, required} from 'react-admin'
+import { useState, useEffect } from "react";
+import {
+  SimpleForm,
+  Edit,
+  TextInput,
+  SelectInput,
+  required
+  //@ts-ignore
+} from "react-admin";
 import config from "../config";
 
-export const AdminEdit= (props:any) => {
-    const [roles, setRoles] = useState([]);
-    useEffect(() => {
-            config.getRolesList().then(data => setRoles(data))
-        }
-    ,[])
-    const choices = roles.map(role => {
-        let choice = {
-            id: role,
-            name: role.charAt(0).toUpperCase()+role.substring(1).toLowerCase();
-    }
-        return choice;
-    });
-    return (
-        <Edit {...props}>
-            <SimpleForm redirect='list'>
-                <TextInput source={'username'} validate={required()} />
-                <TextInput source={'last_name'} />
-                <TextInput source={'first_name'} />
-                <TextInput source={'password'} type={'password'} />
-                <SelectInput source={'role'} choices={choices} validate={required()} />
-            </SimpleForm>
-        </Edit>
-    )
-}
+export const AdminEdit = (props: any) => {
+  const [roles, setRoles] = useState([]);
+  useEffect(() => {
+    config.getRolesList().then(data => setRoles(data));
+  }, []);
+  //@ts-ignore
+  const choices = roles.map(role => {
+    let choice = {
+      id: role,
+      name: role.charAt(0).toUpperCase() + role.substring(1).toLowerCase()
+    };
+    return choice;
+  });
+  return (
+    <Edit {...props}>
+      <SimpleForm redirect="list">
+        <TextInput source={"username"} validate={required()} />
+        <TextInput source={"last_name"} />
+        <TextInput source={"first_name"} />
+        <TextInput source={"password"} type={"password"} />
+        <SelectInput source={"role"} choices={choices} validate={required()} />
+      </SimpleForm>
+    </Edit>
+  );
+};
